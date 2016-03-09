078a36039b598b82c8fc9d56a659e597f41cf489
Daniel Marell <daniel.marell@gmail.com>
2016-03-09 07:23:03 +0100

    More port adjustents.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM java:8-jre
 ADD ["target/dvesta-gateway.jar", "/dvesta-gateway.jar"]
-EXPOSE 10102
+EXPOSE 8093
 CMD ["java", "-jar", "-Dspring.profiles.active=prod", "dvesta-gateway.jar"]
--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ docker rm -f dvesta-gateway-${APP_ENV} &> /dev/null || true
 docker run \
     -e se.marell.dvestagateway.environment=APP_ENV \
     -d --name dvesta-gateway-${APP_ENV} \
-    -p 10102:8080 \
+    -p 10102:8093 \
     dvesta-gateway
